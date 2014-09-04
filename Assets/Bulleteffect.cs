790ff8a194019c499be1ba97e617abaa9fe186de
LudoRathowm <marcolocale@hotmail.it>
2014-09-04 18:43:05 +0400

    project???

---
+++ Assets/Bulleteffect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bulleteffect : MonoBehaviour {
+	Collider2D other;
+	float timer = 2;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	void Update (){
+		if (timer> 0)
+			timer -= Time.deltaTime;
+		if (timer < 0)
+			timer = 0;
+		if (timer == 0) {
+			Destroy(gameObject);
+
+		}
+	}
+	
+	public void	OnTriggerEnter2D(Collider2D other) {
+		GameObject target = other.gameObject;
+		if (target.CompareTag ("Player")) {
+			other.gameObject.GetComponent<PlayerHealth> ().curHealth -= 15;
+			Destroy (gameObject);
+				}
+	
+		}
+	}
+
+
+
---
+++ Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+	
+	private KeyCombo falconPunch= new KeyCombo(new string[] {"down", "right","up"});
+	private KeyCombo falconKick= new KeyCombo(new string[] {"down", "right","Fire1"});
+	private KeyCombo topblock = new KeyCombo (new string[] {"BackHigh", "up", "BackHigh"});
+	void Update () {
+		if (falconPunch.Check())
+		{
+			// do the falcon punch
+			Debug.Log("PUNCH"); 
+		}		
+		if (falconKick.Check())
+		{
+			// do the falcon punch
+			Debug.Log("KICK"); 
+		}
+		if (topblock.Check())
+		{
+			// do the Top block
+			Debug.Log("TOPBLOCK"); 
+		}
+	}
+}
